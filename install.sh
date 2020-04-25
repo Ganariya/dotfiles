c453bec3f382fbe269858ff4c7ff6839ac45caa0
ganariya <willwill1997willwill@yahoo.co.jp>
2020-04-25 16:31:05 +0900

    ok

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+set -u
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-ln -s ${PWD}/.bashrc ~/.bashrc
-ln -s ${PWD}/.bash_profile ~/.bash_profile
-ln -s ${PWD}/.ideavimrc ~/.ideavimrc
-ln -s ${PWD}/.vimrc ~/.vimrc
-ln -s ${PWD}/.config ~/.config
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+    ln -snfv ${PWD}/"$f" ~/
+done
+
