458be3a784bb464056c33c2229e8dba3928bc185
ganariya <ganariya2525@gmail.com>
2021-03-15 12:43:47 +0900

    simple change

--- install_fisher.sh
+++ install_fisher.sh
@@ -4,6 +4,9 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 # fisher
 fisher install oh-my-fish/theme-bobthefish
+fisher install decors/fish-colored-man
+fisher install gazorby/fish-abbreviation-tips
+fisher install laughedelic/pisces
+fisher install acomagu/fish-async-prompt
 fisher install jethrokuan/z
-
-
+fisher install acomagu/fish-async-prompt
