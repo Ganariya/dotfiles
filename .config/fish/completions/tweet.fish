e73fee396d307e1a607e23477fd00b562a884f4c
ganariya <ganariya2525@gmail.com>
2021-03-15 12:29:44 +0900

    erase fish

--- .config/fish/completions/tweet.fish
+++
@@ -1,1 +0,0 @@
-complete --command tweet --no-files --arguments "(env _TWEET_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) tweet)" --condition "env _TWEET_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) tweet"
